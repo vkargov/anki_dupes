8e56f0b837e77b796fcf5f362ad09c0c7cf0d6b6
Vladimir Kargov <kargov@gmail.com>
2018-01-21 22:44:42 -0800

    Fix card movement + general fixes + add changelog

--- anki_dupes.py
+++ anki_dupes.py
@@ -1,22 +1,39 @@
+# -*- coding: utf-8 -*-
+
 # Show duplicates v0.4
 # Show card duplicates for a card below it.
 
-# Changelog
-# 0.4: Disable an overly strict assertion triggering if a card is moved to another deck under certain circumstances.
-# 0.3: Don't group together non-text cards.
-# 0.2: Update cache when the user adds/modifies/deletes cards.
-# 0.1: Initial version.
-
 # TODO
 # Update timings on the duplicates.
-
-# Contact
-# putspamhereplz@gmail.com
+# Remove the duct tape.
+# Replace the chewing gum and putty with glue.
+# Remove the possibility of creating an omnivoracious sentient black hole due to hash collision.
 
 import anki, aqt, hashlib
-from aqt.qt import debug
-from PyQt4.QtCore import pyqtRemoveInputHook
-pyqtRemoveInputHook()
+
+# _debug = True
+
+# if _debug:
+#     from aqt.qt import debug
+#     from PyQt4.QtCore import pyqtRemoveInputHook
+#     import sys
+#     pyqtRemoveInputHook()
+#     _log_file = open('ada.log', 'w')
+#     def _log(s):
+#         _log_file.write(s.encode("utf8") + '\n')
+#         _log_file.flush()
+#     _log("Python version:\n{}".format(sys.version))
+# else:
+#     def _log(s):
+#         pass
+
+
+################################################################################
+
+# TERMINOLOGY
+# {cards} ∈ note, e.g. {仮名, 漢字, 英語} ∈ 日本語
+# or in other words, a "note" is a "unit of knowledge" and a "card" is all the
+# flashcards based on this unit of knowledge
 
 # TABLE cards:
 # nid = note ID
@@ -24,10 +41,23 @@ pyqtRemoveInputHook()
 # ord = ordinal wtf?
 # mod = modified?
 
-# Use the debug() call to put breakpoints wherever you need
+# DEBUGGING
+# Use the debug() call to put breakpoints wherever you need and then
+# just run anki from the console
 
-def gethash(s):
+################################################################################
+
+def _get_hash(s):
     return int(hashlib.md5(s.encode('utf8')).hexdigest(), 16)
+
+def _get_card_hash(col, cid):
+    return _get_hash(_get_card_q(col, cid))
+
+def _get_card_q(col, cid):
+    return anki.utils.stripHTMLMedia(col.renderQA([cid])[0]['q'])
+
+def _get_card_a(col, cid):
+    return anki.utils.stripHTMLMedia(col.renderQA([cid])[0]['a'])
 
 # ada = add dupe answers
 def ada(html, type, fields, model, data, col):
@@ -37,6 +67,9 @@ def ada(html, type, fields, model, data, col):
     # (otherwise something truly horrifying might just happen, but I don't exactly know what)
 
     # renderQA will re-call this hook when rendering each sub-answer.
+
+    # _log("Ada(recursive={})".format(ada.recursive))
+
     if ada.recursive:
         return html
 
@@ -70,7 +103,7 @@ def ada(html, type, fields, model, data, col):
             for card_id in col.db.list("select id from cards where did = %d" % did):
                 # We need a fast way to find answers by the html rendition of their questions.
                 # Creating the hash(question) => {cards} relation with the dictionary should do.
-                h = gethash(anki.utils.stripHTMLMedia(col.renderQA([card_id])[0]['q']))
+                h = _get_card_hash(col, card_id)
                 if h not in ada.QAcache[did]:
                     ada.QAcache[did][h] = []
                 ada.QAcache[did][h].append(card_id)
@@ -84,12 +117,12 @@ def ada(html, type, fields, model, data, col):
                 # don't have time to verify this will not break something else.
                 # TLDR deal with it, at least for now.
 
-                # We also keep the backwards CardID => hash(question) relation needed 
+                # We also keep the backwards CardID => hash(question) relation needed
                 # when we need to dynamically update the main dictionary
                 # when the user adds/edits/deletes cards
                 ada.CardID2Hash[card_id] = h
 
-        h = gethash(ada.question)
+        h = _get_hash(ada.question)
 
         # TODO get rid of this.
         # There are still unexplored cases where we "forget" to rebuild cache.
@@ -108,7 +141,7 @@ def ada(html, type, fields, model, data, col):
                 ada.QAcache[did][h] = [id for id in ada.QAcache[did][h] if id != suspect_card]
                 ada.CardID2Hash.pop(suspect_card, None)
                 continue
-                
+
             suspect_card_qa = col.renderQA([suspect_card])[0]
             if anki.utils.stripHTMLMedia(suspect_card_qa['q']) == ada.question and \
                html != suspect_card_qa['a'] and \
@@ -123,34 +156,54 @@ def ada(html, type, fields, model, data, col):
     return html
 
 # Dynamic updates of our cache when the user adds/modifies/deletes cards
-def UpdateHashes(col, cids):
+# col = collection, cids = card Ids, did = deck ID
+def UpdateHashes(col, cids, did, new_did=None):
+
+    if not new_did:
+        new_did = did
+
+    # _log(u"UpdateHashes (cids = {}, did = {}[{}])".format(str(cids), did, col.decks.get(did)['name']))
     for cid in cids:
-        for cid, did in col.db.execute("select id, did from cards where nid = ? order by ord", cid):
-            if cid in ada.CardID2Hash:
-                # Modified/deleted card has existed, wipe it from our cache
-                ada.QAcache[did][ada.CardID2Hash[cid]] = [id for id in ada.QAcache[did][ada.CardID2Hash[cid]] if id != cid]
-            h = gethash(anki.utils.stripHTMLMedia(col.renderQA([cid])[0]['q']))
-            if h not in ada.QAcache[did]:
-                ada.QAcache[did][h] = []
-            ada.QAcache[did][h].append(cid)
-            ada.CardID2Hash[cid] = h
+        # _log(u"  Processing {} [{}]".format(cid, _get_card_q(col, cid).strip()))
+
+        # Purge from the old location
+        if cid in ada.CardID2Hash:
+            # _log(u"    QAcache before: {}({})".format(ada.CardID2Hash[cid], str(ada.QAcache[did][ada.CardID2Hash[cid]])))
+            # Modified/deleted card has existed, wipe it from our cache
+            ada.QAcache[did][ada.CardID2Hash[cid]] = [id for id in ada.QAcache[did][ada.CardID2Hash[cid]] if id != cid]
+            # _log(u"    QAcache after: {}".format(str(ada.QAcache[did][ada.CardID2Hash[cid]])))
+
+        h = _get_card_hash(col, cid)
+        if h not in ada.QAcache[new_did]:
+            ada.QAcache[new_did][h] = []
+        ada.QAcache[new_did][h].append(cid)
+        ada.CardID2Hash[cid] = h
 
 # Dynamic updates of our cache when the user adds/modifies/deletes cards
 def UpdateNoteHashes(self):
-    UpdateHashes(self.col, [self.id])
-        
+    cards = self.cards();
+    if not cards:
+        return
+    UpdateHashes(self.col, [card.id for card in self.cards()], cards[0].did)
+
 # When the user adds a card, new cards are not readily available for modification during note.flush().
 # TODO: perhaps updating only cards will suffice?
 def UpdateCardHashes(self):
-    UpdateNoteHashes(self.note())
+    UpdateHashes(self.col, [self.id], self.did)
+
+def BrowserCid2Did(self, cid):
+    return self.mw.col.db.scalar("select did from cards where id = ?", cid)
 
 def PreUpdateSelectedCardHashes(self):
-    UpdateSelectedCardHashes.cids = self.selectedCards()
+    cids = self.selectedCards()
+    UpdateSelectedCardHashes.old_did = BrowserCid2Did(self, cids[0])
 
 def UpdateSelectedCardHashes(self):
-    UpdateHashes(self.mw.col, UpdateSelectedCardHashes.cids)
-    UpdateHashes(self.mw.col, self.selectedCards())
-    del UpdateSelectedCardHashes.cids
+    cids = self.selectedCards()
+    old_did = UpdateSelectedCardHashes.old_did
+    new_did = BrowserCid2Did(self, cids[0])
+    UpdateHashes(self.mw.col, cids, old_did, new_did)
+    del UpdateSelectedCardHashes.old_did
 
 ada.QAcache = {}
 ada.CardID2Hash = {}
